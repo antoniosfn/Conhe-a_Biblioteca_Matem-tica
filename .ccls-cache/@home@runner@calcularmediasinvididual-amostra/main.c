b458764c1db5e2f9beadbd54fcfadc36c0b580fb
Antonio Neto <117511186+antoniosfn@users.noreply.github.com>
2023-04-01 09:31:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@calcularmediasinvididual-amostra/main.c
@@ -0,0 +1,9 @@
+/* Vamos */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+/* 
+ © 2023 Professor Antonio Neto <netontn2023@gmail.com>
+ Projeto pessoal de Ensino de matemática e Prática em C
+ Fonte: https://petbcc.ufscar.br/math/
+*/
+
+/* Declarando as bibliotecas: 1-Entrada/saída de informações */
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+  printf("Sejam todos bem-vindos ao meu portfolio! Eu sou o professor antonio neto e hoje vou tentar repassar meu conhecimento me desenvolvo em C e revisando conceitos matemáticos com vocês.\n");
+  return 0;
+
+}
